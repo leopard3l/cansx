de84fb4d310e9f8b54a096f0c594c1e9f7043aff
leopard3l <63955890+leopard3l@users.noreply.github.com>
2022-02-06 17:00:31 +0100

    Update receive_data.py
    Remove autoacknowledge

--- receive_data.py
+++ receive_data.py
@@ -13,7 +13,7 @@ print ("Starting reception program")
 # The following are for an Adafruit RFM69HCW Transceiver Radio
 # Bonnet https://www.adafruit.com/product/4072
 # You should adjust them to whatever matches your radio
-with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=True, verbose=False, interruptPin=18, resetPin=22, spiDevice=0, autoAcknowledge=True) as radio:
+with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=True, verbose=False, interruptPin=18, resetPin=22, spiDevice=0, autoAcknowledge=False) as radio:
     print ("Starting receiving loop...")
     radio.calibrate_radio()
     radio.set_power_level(100)

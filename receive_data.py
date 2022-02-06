1a620570679dffe40a747aa0f4de86c387e2ba97
leopard3l <63955890+leopard3l@users.noreply.github.com>
2022-02-06 16:44:07 +0100

    Create receive_data.py

---
+++ receive_data.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import time
+from RFM69 import Radio, FREQ_433MHZ
+
+node_id = 2
+network_id = 100
+recipient_id = 1
+number = 0
+
+print ("Starting reception program")
+
+# The following are for an Adafruit RFM69HCW Transceiver Radio
+# Bonnet https://www.adafruit.com/product/4072
+# You should adjust them to whatever matches your radio
+with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=True, verbose=False, interruptPin=18, resetPin=22, spiDevice=0, autoAcknowledge=True) as radio:
+    print ("Starting receiving loop...")
+    radio.calibrate_radio()
+    radio.set_power_level(100)
+    radio.set_frequency_in_kHz(434000000)
+
+    while True:
+        number += 1
+
+        packet = radio.get_packet()
+
+        # If radio.get_packet times out, it will return None
+        if packet is not None:
+            datas = packet.to_dict()
+            print("Message %s, RSSI %s= %s" % (str(number), datas['rssi'], packet.data_string))

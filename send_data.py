e08042876b2ad6051352d2097f0e1b31a4c69430
leopard3l <63955890+leopard3l@users.noreply.github.com>
2022-02-06 17:00:53 +0100

    Update send_data.py
    Remove autoacknowledge

--- send_data.py
+++ send_data.py
@@ -14,7 +14,7 @@ print ("Starting transmission program")
 # The following are for an Adafruit RFM69HCW Transceiver Radio
 # Bonnet https://www.adafruit.com/product/4072
 # You should adjust them to whatever matches your radio
-with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=True, verbose=False, interruptPin=18, resetPin=22, spiDevice=0, autoAcknowledge=True) as radio:
+with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=True, verbose=False, interruptPin=18, resetPin=22, spiDevice=0, autoAcknowledge=False) as radio:
     print ("Starting sending loop...")
     radio.calibrate_radio()
     radio.set_power_level(100)

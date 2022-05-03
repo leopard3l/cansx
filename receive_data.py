6fd63da0d34ee764f31ef483576382c907159353
leopard3l <63955890+leopard3l@users.noreply.github.com>
2022-05-03 02:30:02 +0200

    Update receive_data.py

--- receive_data.py
+++ receive_data.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python
 
 import time
+import RPi.GPIO as GPIO  
 from RFM69 import Radio, FREQ_433MHZ
 
-node_id = 2
-network_id = 100
-recipient_id = 1
+frequency = 434000000 
+myEncryptionKey = "cansxINDA"
 number = 0
 
 print ("Starting reception program")
 
-with Radio(FREQ_433MHZ, node_id, network_id, isHighPower=True, verbose=False, interruptPin=18, resetPin=22, spiDevice=0, autoAcknowledge=False) as radio:
-    print ("Starting receiving loop...")
-
-    radio.calibrate_radio()
-    radio.set_power_level(100)
-    radio.set_frequency_in_kHz(434000000)
-
-    while True:
-        number += 1
-
-        packet = radio.get_packet()
-
-        # If radio.get_packet times out, it will return None
-        if packet is not None:
-            datas = packet.to_dict()
-            print("Message %s, RSSI %s= %s" % (str(number), datas['rssi'], packet.data_string))
+try:
+    with Radio(FREQ_433MHZ, nodeID=2, networkID=100, isHighPower=True, verbose=False, interruptPin=18, resetPin=22, spiDevice=0, autoAcknowledge=False, encryptionKey=myEncryptionKey) as radio, open('database.csv', 'a+') as output:
+        print ("Starting receiving data...")
+    
+        radio.calibrate_radio()
+        radio.set_power_level(100)
+        radio.set_frequency_in_Hz(frequency)
+    
+        while True:
+            number += 1
+    
+            packet = radio.get_packet(timeout=1)
+    
+            if packet is not None:
+                datas = packet.to_dict()
+                print("Message %s, RSSI %s= %s" % (str(number), datas['rssi'], packet.data_string))
+                print("%s,%s,%s" % (str(number), datas['rssi'], packet.data_string), file=output)
+finally:
+    GPIO.cleanup()
